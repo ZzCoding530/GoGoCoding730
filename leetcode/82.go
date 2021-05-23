e7b3e483cda9f2254b8a0a74d780e10648c1ad50
zzcoding530 <rookiezc@163.com>
2021-05-23 22:47:53 +0800

    82 删除链表重复元素II

---
+++ leetcode/82.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	dummy := &ListNode{0, head}
+
+	cur := dummy
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			x := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == x {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return dummy.Next
+}
