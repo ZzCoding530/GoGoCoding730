c08c63f3d1e5e8f672604bcc96003225e877ee27
zzcoding530 <rookiezc@163.com>
2021-05-31 14:19:00 +0800

    98 验证搜素二叉树的迭代法

--- leetcode/98-2.go
+++ leetcode/98-2.go
@@ -1,14 +1,42 @@
 package leetcode
 
-var list *[]int = new([]int)
-
+/*
+	迭代法，其实指的是层序遍历的迭代版本， 如果是搜索二叉树，层序遍历应该是单调递增的
+*/
 func isValidBST(root *TreeNode) bool {
 
 	if root == nil {
 		return true
 	}
-	list = inorder(root)
 
+	stack := []*TreeNode{root}
+	result := []int{}
+
+	for len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if len(result) > 1 {
+			if isOrder(result) == false {
+				return false
+			}
+		}
+
+		result = append(result, root.Val)
+
+		root = root.Right
+	}
+
+	return true
+
+}
+
+func isOrder(list []int) bool {
 	for i := 1; i < len(list); i++ {
 		if list[i] <= list[i-1] {
 			return false
@@ -17,13 +45,4 @@ func isValidBST(root *TreeNode) bool {
 
 	return true
 
-}
-
-func inorder(root *TreeNode) []int {
-	if root != nil {
-		inorder(root.Left)
-		list = append(list, root.Val)
-		inorder(root.Right)
-	}
-	return list
 }
