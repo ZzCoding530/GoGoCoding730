c99ea2402d0f804b98bdca4ad04371b5e56eb941
zzcoding530 <rookiezc@163.com>
2021-06-02 13:25:03 +0800

    232 用栈实现队列

---
+++ leetcode/232.go
@@ -0,0 +1,40 @@
+package leetcode
+
+type MyQueue struct {
+	inStack, outStack []int
+}
+
+func Constructor() MyQueue {
+	return MyQueue{}
+}
+
+func (q *MyQueue) Push(x int) {
+	q.inStack = append(q.inStack, x)
+}
+
+func (q *MyQueue) in2out() {
+	for len(q.inStack) > 0 {
+		q.outStack = append(q.outStack, q.inStack[len(q.inStack)-1])
+		q.inStack = q.inStack[:len(q.inStack)-1]
+	}
+}
+
+func (q *MyQueue) Pop() int {
+	if len(q.outStack) == 0 {
+		q.in2out()
+	}
+	x := q.outStack[len(q.outStack)-1]
+	q.outStack = q.outStack[:len(q.outStack)-1]
+	return x
+}
+
+func (q *MyQueue) Peek() int {
+	if len(q.outStack) == 0 {
+		q.in2out()
+	}
+	return q.outStack[len(q.outStack)-1]
+}
+
+func (q *MyQueue) Empty() bool {
+	return len(q.inStack) == 0 && len(q.outStack) == 0
+}
