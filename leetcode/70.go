142ac6bde957dcf3b2ade258ea7acca3ad0b7f63
zzcoding530 <rookiezc@163.com>
2021-06-03 17:07:45 +0800

    70 爬楼梯

---
+++ leetcode/70.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func climbStairs(n int) int {
+	if n < 2 {
+		return n
+	}
+	dp := make([]int, n+1)
+
+	dp[1] = 1
+	dp[2] = 2
+
+	for i := 3; i < len(dp); i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
