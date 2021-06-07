9250933d180e0d8c09ec50a218d3871a03390175
zzcoding530 <rookiezc@163.com>
2021-06-07 11:39:18 +0800

    1143 最长公共子序列

---
+++ leetcode/1143.go
@@ -0,0 +1,32 @@
+package leetcode
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	l1 := len(text1)
+	l2 := len(text2)
+
+	dp := make([][]int, l1+1)
+
+	for i := range dp {
+		dp[i] = make([]int, l2+1)
+	}
+
+	for i := 1; i < l1+1; i++ {
+		for j := 1; j < l2+1; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[l1][l2]
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	} else {
+		return j
+	}
+}
