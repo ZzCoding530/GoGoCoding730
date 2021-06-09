374231d48cf308bf660af9229c94192cd518915c
zzcoding530 <rookiezc@163.com>
2021-06-09 20:56:05 +0800

    旋转图像

---
+++ leetcode/0048.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	// 水平翻转
+	for i := 0; i < n/2; i++ {
+		matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+	}
+	// 主对角线翻转
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
