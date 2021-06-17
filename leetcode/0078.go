c051d93663408d37c6f60bb8ac1446d26dee9ed7
zzcoding530 <rookiezc@163.com>
2021-06-17 23:08:35 +0800

    添加了注释

--- leetcode/0078.go
+++ leetcode/0078.go
@@ -5,13 +5,14 @@ func subsets(nums []int) (ans [][]int) {
 	var dfs func(int)
 	dfs = func(cur int) {
 		if cur == len(nums) { //全都遍历了 返回吧
-			ans = append(ans, append([]int(nil), set...))
+			ans = append(ans, append([]int(nil), set...)) //append([]int(nil), set...)这个写法相当于是新建一个空切片然后把set当前的值复制进去，避免下一轮回溯set变了
 			return
 		}
-		set = append(set, nums[cur])
-		dfs(cur + 1)
-		set = set[:len(set)-1]
-		dfs(cur + 1)
+
+		set = append(set, nums[cur]) //首先把当前值放进set
+		dfs(cur + 1)                 //然后下一个值
+		set = set[:len(set)-1]       //然后把刚才放进去的拿出来
+		dfs(cur + 1)                 //重复刚才的
 	}
 	dfs(0)
 	return
