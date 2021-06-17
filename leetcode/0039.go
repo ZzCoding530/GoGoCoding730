8faa0bc422582756a51f6d447611c6955a8f7f68
zzcoding530 <rookiezc@163.com>
2021-06-17 23:22:40 +0800

    39更新注释

--- leetcode/0039.go
+++ leetcode/0039.go
@@ -1,25 +1,25 @@
 package leetcode
 
 func combinationSum(candidates []int, target int) (ans [][]int) {
-	comb := []int{} //目前的组合
+	comb := []int{}
 	var dfs func(target, idx int)
 	dfs = func(target, idx int) {
-		if idx == len(candidates) {
+		if idx == len(candidates) { //走到最后一个数了 return
 			return
 		}
-		if target == 0 { //树遍历到底了
+		if target == 0 { //已经和为target了 return当前的组合
 			ans = append(ans, append([]int(nil), comb...))
 			return
 		}
-		// 直接跳过
-		dfs(target, idx+1)
-		// 选择当前数
-		if target-candidates[idx] >= 0 {
+
+		if target-candidates[idx] >= 0 { // 选择当前数
 			comb = append(comb, candidates[idx])
 			dfs(target-candidates[idx], idx)
 			comb = comb[:len(comb)-1]
 		}
+
+		dfs(target, idx+1) //跳过当前数
 	}
 	dfs(target, 0)
-	return ans
+	return //返回值是有名字的就不用在函数内返回了
 }
