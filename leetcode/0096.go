1cf24f5600ce50b06b461da9a90e73b8e1b41bac
zzcoding530 <rookiezc@163.com>
2021-06-29 17:29:35 +0800

    不同的二叉搜索树

---
+++ leetcode/0096.go
@@ -0,0 +1,18 @@
+package leetcode
+
+/*
+	看题解
+*/
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+
+	for i := 2; i < n+1; i++ {
+		for j := 1; j < i+1; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+
+	return dp[n]
+}
