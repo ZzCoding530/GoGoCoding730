458f3bd93faa47dc7a28d9ca5718be19d1ebf551
zzcoding530 <rookiezc@163.com>
2021-06-29 15:54:41 +0800

    颜色分类

---
+++ leetcode/0075.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func sortColors(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+
+}
