e1a286bb4a065f8e0d89e3b6d59d0bca8851d794
zzcoding530 <rookiezc@163.com>
2021-07-02 17:01:47 +0800

    长度最小的子数组

---
+++ leetcode/0209.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import "math"
+
+func minSubArrayLen(s int, nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	ans := math.MaxInt32
+	start, end := 0, 0
+	sum := 0
+	for end < n {
+		sum += nums[end]
+		for sum >= s {
+			ans = min(ans, end-start+1)
+			sum -= nums[start]
+			start++
+		}
+		end++
+	}
+	if ans == math.MaxInt32 {
+		return 0
+	}
+	return ans
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
