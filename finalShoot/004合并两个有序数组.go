f351d36f2b782cc2101183d45042f131916aebd9
zzcoding530 <rookiezc@163.com>
2021-07-18 20:57:27 +0800

    88

---
+++ finalShoot/004合并两个有序数组.go
@@ -0,0 +1,26 @@
+package finalShoot
+
+/*
+	挺高频的一道题
+	从后往前指针走，从后往前确定数组每一个格子的数
+	https://leetcode-cn.com/problems/merge-sorted-array/solution/88-by-ikaruga/
+	这个题解非常明白
+*/
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	index := len(nums1) - 1
+	m--
+	n--
+
+	for n >= 0 { //用for循环m和n吧，循环index的话，-1不好处理
+		for m >= 0 && nums1[m] > nums2[n] {
+			nums1[index], nums1[m] = nums1[m], nums1[index]
+			index--
+			m--
+		}
+
+		nums1[index], nums2[n] = nums2[n], nums1[index]
+		index--
+		n--
+	}
+
+}
