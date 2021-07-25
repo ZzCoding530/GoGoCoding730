b7dc542d53a67cc3a5ec74b8b9fb1674c43f5755
zzcoding530 <rookiezc@163.com>
2021-07-25 17:32:18 +0800

    dp

---
+++ finalShoot/018买卖股票的最佳时机II.go
@@ -0,0 +1,19 @@
+package finalShoot
+
+func maxProfit(prices []int) int {
+	n := len(prices)
+	dp := make([][2]int, n)
+	dp[0][1] = -prices[0]
+	for i := 1; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+	}
+	return dp[n-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
