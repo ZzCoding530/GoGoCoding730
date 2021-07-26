1eb1935319a95f8b6d82f2c42a13d86b2eb2e1f8
zzcoding530 <rookiezc@163.com>
2021-07-26 14:49:13 +0800

    105

---
+++ finalShoot/022从前序与中序遍历中重建二叉树.go
@@ -0,0 +1,25 @@
+package finalShoot
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{
+		Val:   preorder[0],
+		Left:  nil,
+		Right: nil,
+	}
+
+	i := 0                        //i 要定义在外面，因为for循环后还要用
+	for ; i < len(inorder); i++ { //从inorder数组中找到preorder[0]的位置
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+
+	root.Left = buildTree(preorder[1:len(inorder[:i])+1], inorder[:i])
+	root.Right = buildTree(preorder[len(inorder[:i])+1:], inorder[i+1:])
+	return root
+
+}
