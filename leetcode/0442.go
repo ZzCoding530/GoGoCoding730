c8cf77adbdb18d73154a0208303983e97c03371b
zzcoding530 <rookiezc@163.com>
2021-08-01 00:05:00 +0800

    数组中重复出现的元素

--- leetcode/0442.go
+++ leetcode/0442.go
@@ -5,11 +5,11 @@ func findDuplicates(nums []int) []int {
 	n := len(nums)
 
 	//有一个前提：数组里的数都是 1 - n 的
-	for i := 0; i < n; i++ { // 比如现在index = i ，nums[i] = x , 那么让nums[x-1]取反； 如果已经是负数了，那说明 x 已经出现过一次了
+	for i := 0; i < n; i++ { // 比如现在index = i ，nums[i] = x , 那么让nums[x-1]置为负值； 如果已经是负数了，那说明 x 已经出现过一次了
 		if nums[abs(nums[i])-1] <= 0 {
 			res = append(res, abs(nums[i]))
 		} else {
-			nums[abs(nums[i])-1] *= -1
+			nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1])
 		}
 	}
 
