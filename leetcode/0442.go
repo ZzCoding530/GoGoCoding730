842c38c61af61b032b1301b1b02cee7ab2a81cdf
zzcoding530 <rookiezc@163.com>
2021-07-31 23:54:17 +0800

    数组中重复的数据

---
+++ leetcode/0442.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func findDuplicates(nums []int) []int {
+	res := []int{}
+	n := len(nums)
+
+	//有一个前提：数组里的数都是 1 - n 的
+	for i := 0; i < n; i++ { // 比如现在index = i ，nums[i] = x , 那么让nums[x-1]取反； 如果已经是负数了，那说明 x 已经出现过一次了
+		if nums[abs(nums[i])-1] <= 0 {
+			res = append(res, abs(nums[i]))
+		} else {
+			nums[abs(nums[i])-1] *= -1
+		}
+	}
+
+	return res
+
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
