69eb9129fb5de2c24bf77315a07c888d29e2eea5
zzcoding530 <rookiezc@163.com>
2021-08-10 16:25:02 +0800

    309

---
+++ finalShoot/037买卖股票的最佳时机含冷冻期.go
@@ -0,0 +1,30 @@
+package finalShoot
+
+/*
+	官方题解足够好
+	https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/solution/zui-jia-mai-mai-gu-piao-shi-ji-han-leng-dong-qi-4/
+*/
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	n := len(prices)
+	// f[i][0]: 手上持有股票的最大收益
+	// f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+	// f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+	f := make([][3]int, n)
+	f[0][0] = -prices[0]
+	for i := 1; i < n; i++ {
+		f[i][0] = max(f[i-1][0], f[i-1][2]-prices[i])
+		f[i][1] = f[i-1][0] + prices[i]
+		f[i][2] = max(f[i-1][1], f[i-1][2])
+	}
+	return max(f[n-1][1], f[n-1][2])
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
